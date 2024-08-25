d0e9f9670fbb9f125032921b17751a39d990a5f8
Omar J <100746749+omsinj@users.noreply.github.com>
2024-08-25 22:27:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import React, { useState } from 'react';
 import ProductList from './ProductList';
-import AboutUs from './AboutUs';
-import CartItem from './CartItem'; // Import the CartItem component
 import './App.css';
+import AboutUs from './AboutUs';
 
 function App() {
+  
+  const [showProductList, setShowProductList] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
   return (
-    <Router>
-      <div className="app-container">
-        <header className="header">
-          <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/products">Products</Link></li>
-              <li><Link to="/cart">Cart</Link></li>
-            </ul>
-          </nav>
-        </header>
+    <div className="app-container">
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+        <div className="background-image"></div>
+        <div className="content">
+         <div className="landing_content">
+         <h1>Welcome To Paradise Nursery</h1>
+          <div className="divider"></div>
+          <p>Where Green Meets Serenity</p>
+         
+          <button className="get-started-button" onClick={handleGetStartedClick}>
+            Get Started
+          </button>
+         </div>
+          <div className="aboutus_container">
+          <AboutUs/>
+          </div>
+          </div>
 
-        <Routes>
-          {/* Landing Page Route */}
-          <Route
-            path="/"
-            element={
-              <div className="landing-page">
-                <div className="background-image"></div>
-                <div className="content">
-                  <div className="landing_content">
-                    <h1>Welcome To Paradise Nursery</h1>
-                    <div className="divider"></div>
-                    <p>Where Green Meets Serenity</p>
-                    <Link to="/products">
-                      <button className="get-started-button">Get Started</button>
-                    </Link>
-                  </div>
-                  <div className="aboutus_container">
-                    <AboutUs />
-                  </div>
-                </div>
-              </div>
-            }
-          />
-
-          {/* Product List Page Route */}
-          <Route path="/products" element={<ProductList />} />
-
-          {/* Cart Page Route */}
-          <Route path="/cart" element={<CartItem />} />
-
-          {/* Additional Pages can be added here */}
-          {/* <Route path="/about" element={<AboutUs />} /> */}
-        </Routes>
       </div>
-    </Router>
+      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+        <ProductList />
+      </div>
+    </div>
   );
 }
 
