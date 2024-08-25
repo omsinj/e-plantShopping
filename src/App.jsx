17a15b273deb9e853ef85d1eab57af19360a11e0
Omar J <100746749+omsinj@users.noreply.github.com>
2024-08-25 20:46:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,45 @@
-
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ProductList from './ProductList';
-import './App.css';
 import AboutUs from './AboutUs';
+import './App.css';
 
 function App() {
-  
-  const [showProductList, setShowProductList] = useState(false);
-
-  const handleGetStartedClick = () => {
-    setShowProductList(true);
-  };
-
   return (
-    <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
+    <Router>
+      <div className="app-container">
+        <Routes>
+          {/* Landing Page Route */}
+          <Route
+            path="/"
+            element={
+              <div className="landing-page">
+                <div className="background-image"></div>
+                <div className="content">
+                  <div className="landing_content">
+                    <h1>Welcome To Paradise Nursery</h1>
+                    <div className="divider"></div>
+                    <p>Where Green Meets Serenity</p>
+                    <Link to="/products">
+                      <button className="get-started-button">Get Started</button>
+                    </Link>
+                  </div>
+                  <div className="aboutus_container">
+                    <AboutUs />
+                  </div>
+                </div>
+              </div>
+            }
+          />
 
+          {/* Product List Page Route */}
+          <Route path="/products" element={<ProductList />} />
+
+          {/* Additional Pages can be added like this */}
+          {/* <Route path="/about" element={<AboutUs />} /> */}
+        </Routes>
       </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList />
-      </div>
-    </div>
+    </Router>
   );
 }
 
